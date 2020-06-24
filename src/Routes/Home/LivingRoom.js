2ad37cb23e96e9a1ec84cb366198529bc9226d0c
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-06-24 18:36:18 -0500

    added ceiling thing

--- src/Routes/Home/LivingRoom.js
+++ src/Routes/Home/LivingRoom.js
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import axios from 'axios'
-import { SketchPicker } from 'react-color';
+import axios from "axios";
+import { SketchPicker } from "react-color";
 
 const LivingRoom = () => {
   const [musicState, setMusicState] = useState({
     color: "#808080",
     isToggled: false,
   });
-  const [lightColorState,setLightColorState] = useState({couchColor:"#e8e8e8"})
+  const [lightColorState, setLightColorState] = useState({
+    couchColor: "#e8e8e8",
+    ceilingColor:"#e8e8e8"
+  });
+  const [message, setMessage] = useState({message:""});
+
+
+
   const ToggleMusicModeButton = styled.button`
-  background:${musicState.color}
+    background: ${musicState.color};
   `;
 
-  const CouchPostButton = styled.button`
-  background:${lightColorState.couchColor}
+  const CouchContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+
   `
+  const CouchPostButton = styled.button`
+    background: ${lightColorState.couchColor};
+  `;
+  const LivingRoomCeilingContainer = styled.div`
+    display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  
+  `
+
+
   const toggleMusicMode = () => {
     axios
       .post(`http://localhost:5555/musictoggle`, {})
@@ -28,28 +51,60 @@ const LivingRoom = () => {
       })
       .catch((err) => {
         setMusicState({ color: "#808080", isToggled: false });
-
       });
   };
-  const updateLightColor = (url,color) => {
-    console.log(url,color)
-    axios.get(`${url}/${color}`)
-  }
-  const handleCouchColorChange = (color) =>{
-    setLightColorState({...lightColorState,couchColor: color.hex})
+
+  const updateLightColor = (url, color) => {
+    console.log(url, color);
+    axios
+      .get(`${url}/${color}`)
+      .then((res) => {
+        setMessage({message: `Successfully Set Lights To ${color}`});
+      })
+      .catch((err) => {
+        setMessage({message:`Failed To Set Lights To ${color} because of ${err.message}`});
+      });
+  };
+  const handleCouchColorChange = (color) => {
+    setLightColorState({ ...lightColorState, couchColor: color.hex });
+  };
+  const handleCeilingChange = (color) => {
+    setLightColorState({...lightColorState, ceilingColor:color.hex })
   }
   return (
     <div>
+      <h3>{message.message}</h3>
       <div>
-      <h3>Toggle on the music</h3>
-      <ToggleMusicModeButton onClick={toggleMusicMode} >Toggle Music </ToggleMusicModeButton>
+        <h3>Toggle on the music</h3>
+        <ToggleMusicModeButton onClick={toggleMusicMode}>
+          Toggle Music{" "}
+        </ToggleMusicModeButton>
       </div>
-      <div>
+      <CouchContainer>
         <h3>Change Couch Light Colors</h3>
-          <SketchPicker color={lightColorState.couchColor} onChange={handleCouchColorChange}/>
-          <CouchPostButton onClick={() =>updateLightColor(`http://localhost:5555/lights/couch`,lightColorState.couchColor)}>Change Lights</CouchPostButton>
-      </div>
-
+        <SketchPicker
+          color={lightColorState.couchColor}
+          onChange={handleCouchColorChange}
+        />
+        <CouchPostButton
+          onClick={() =>
+            updateLightColor(
+              `http://localhost:5555/lights/couch`,
+              lightColorState.couchColor
+            )
+          }
+        >
+          Change Lights
+        </CouchPostButton>
+        
+      </CouchContainer>
+      <LivingRoomCeilingContainer>
+        <h3>Change Ceiling Lights</h3>
+        <SketchPicker
+        color={lightColorState.ceilingColor}
+        onChange={handleCeilingChange}
+        />
+      </LivingRoomCeilingContainer>
     </div>
   );
 };
