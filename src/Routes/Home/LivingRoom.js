bf119189b939e20690e423a002bf77c5e11b9829
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-06-24 23:40:58 -0500

    completed living room route

--- src/Routes/Home/LivingRoom.js
+++ src/Routes/Home/LivingRoom.js
@@ -10,33 +10,11 @@ const LivingRoom = () => {
   });
   const [lightColorState, setLightColorState] = useState({
     couchColor: "#e8e8e8",
-    ceilingColor:"#e8e8e8"
+    ceilingColor:"#e8e8e8",
+    stairsColor:"#e8e8e8",
+    floorColor:"#e8e8e8"
   });
   const [message, setMessage] = useState({message:""});
-
-
-
-  const ToggleMusicModeButton = styled.button`
-    background: ${musicState.color};
-  `;
-
-  const CouchContainer = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-
-  `
-  const CouchPostButton = styled.button`
-    background: ${lightColorState.couchColor};
-  `;
-  const LivingRoomCeilingContainer = styled.div`
-    display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  
-  `
 
 
   const toggleMusicMode = () => {
@@ -65,21 +43,95 @@ const LivingRoom = () => {
         setMessage({message:`Failed To Set Lights To ${color} because of ${err.message}`});
       });
   };
+
+
   const handleCouchColorChange = (color) => {
     setLightColorState({ ...lightColorState, couchColor: color.hex });
   };
   const handleCeilingChange = (color) => {
     setLightColorState({...lightColorState, ceilingColor:color.hex })
   }
+  const handleStairsChange = color => {
+    setLightColorState({...lightColorState,stairsColor:color.hex})
+  }
+  const handleFloorChange = color => {
+    setLightColorState({...lightColorState,floorColor:color.hex})
+  }
+  
+
+  const Container = styled.div`
+  display:flex;
+  justify-content:space-around;
+  `
+
+  const ToggleMusicModeButton = styled.button`
+    background: ${musicState.color};
+  `;
+  const StairsContianer = styled.div`
+  width:50vw;
+    display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  background:${lightColorState.stairsColor};
+  `
+  const CouchContainer = styled.div`
+    width:50vw;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  background:${lightColorState.couchColor};
+
+  `
+
+  const CouchPostButton = styled.button`
+    border: 2px black solid;
+    background:white;
+    color: ${lightColorState.couchColor};
+  `;
+
+  const LivingRoomCeilingContainer = styled.div`
+    width:50vw;
+  background:${lightColorState.ceilingColor};
+    display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  
+  `
+  const CeilingPostButton = styled.button`
+      border: 2px black solid;
+    background:white;
+  color:${lightColorState.ceilingColor};
+  
+  `
+
+const StairsPostButton = styled.button`
+      border: 2px black solid;
+    background:white;
+  color:${lightColorState.stairsColor};
+`
+
+const FloorContainer = styled.div`
+  width:50vw;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+background:${lightColorState.floorColor};
+`
+const FloorPostButton = styled.button`
+      border: 2px black solid;
+    background:white;
+  color:${lightColorState.floorColor};
+`
+
   return (
     <div>
       <h3>{message.message}</h3>
-      <div>
-        <h3>Toggle on the music</h3>
-        <ToggleMusicModeButton onClick={toggleMusicMode}>
-          Toggle Music{" "}
-        </ToggleMusicModeButton>
-      </div>
+
+      <Container>
       <CouchContainer>
         <h3>Change Couch Light Colors</h3>
         <SketchPicker
@@ -98,13 +150,51 @@ const LivingRoom = () => {
         </CouchPostButton>
         
       </CouchContainer>
+      <StairsContianer>
+        <h3>Change Stair Lights</h3>
+        <SketchPicker
+        color={lightColorState.stairsColor}
+        onChange={handleStairsChange}
+        />
+        <StairsPostButton
+        onClick={() => updateLightColor(`http://localhost:5555/lights/stairs`,lightColorState.stairsColor)}
+        >
+          Change Lights
+        </StairsPostButton>
+      </StairsContianer>
+      </Container>
+      <Container>
       <LivingRoomCeilingContainer>
         <h3>Change Ceiling Lights</h3>
         <SketchPicker
         color={lightColorState.ceilingColor}
         onChange={handleCeilingChange}
         />
+        <CeilingPostButton
+        onClick={() => updateLightColor(`http://localhost:5555/lights/livingroominceiling`, lightColorState.ceilingColor)}
+        >
+          Change Lights
+        </CeilingPostButton>
       </LivingRoomCeilingContainer>
+      <FloorContainer>
+        <h3>Change Floor Lights</h3>
+        <SketchPicker
+        color={lightColorState.floorColor}
+        onChange={handleFloorChange}
+        />
+        <FloorPostButton
+        onClick={() => updateLightColor(`http://localhost:5555/lights/floor`,lightColorState.floorColor)}
+        >
+        Change Lights
+        </FloorPostButton>
+      </FloorContainer>
+      </Container>
+      <div>
+        <h3>Toggle on the music</h3>
+        <ToggleMusicModeButton onClick={toggleMusicMode}>
+          Toggle Music{" "}
+        </ToggleMusicModeButton>
+      </div>
     </div>
   );
 };
