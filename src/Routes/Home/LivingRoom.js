badd2e3affd9829e49f5ef64b7e9df0ce30b2aee
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-07-02 04:35:01 -0500

    redoing the server in node

--- src/Routes/Home/LivingRoom.js
+++ src/Routes/Home/LivingRoom.js
@@ -9,13 +9,12 @@ const LivingRoom = () => {
     isToggled: false,
   });
   const [lightColorState, setLightColorState] = useState({
-    couchColor: "#e8e8e8",
-    ceilingColor:"#e8e8e8",
-    stairsColor:"#e8e8e8",
-    floorColor:"#e8e8e8"
+    couchColor: "e8e8e8",
+    ceilingColor: "e8e8e8",
+    stairsColor: "e8e8e8",
+    floorColor: "e8e8e8",
   });
-  const [message, setMessage] = useState({message:""});
-
+  const [message, setMessage] = useState({ message: "" });
 
   const toggleMusicMode = () => {
     axios
@@ -33,168 +32,180 @@ const LivingRoom = () => {
   };
 
   const updateLightColor = (url, color) => {
+    console.log("Color", color);
     console.log(`${url}/${color}`);
-    
+
     axios
       .get(`${url}/${color}`)
       .then((res) => {
-        setMessage({message: `Successfully Set Lights To ${color}`});
+        console.log(res, "res");
+        setMessage({ message: `Successfully Set Lights To ${color}` });
       })
       .catch((err) => {
-        setMessage({message:`Failed To Set Lights To ${color} because of ${err.message}`});
+        setMessage({
+          message: `Failed To Set Lights To ${color} because of ${err.message}`,
+        });
       });
   };
-
 
   const handleCouchColorChange = (color) => {
     setLightColorState({ ...lightColorState, couchColor: color.hex });
   };
   const handleCeilingChange = (color) => {
-    setLightColorState({...lightColorState, ceilingColor:color.hex })
-  }
-  const handleStairsChange = color => {
-    setLightColorState({...lightColorState,stairsColor:color.hex})
-  }
-  const handleFloorChange = color => {
-    setLightColorState({...lightColorState,floorColor:color.hex})
-  }
-  
+    setLightColorState({ ...lightColorState, ceilingColor: color.hex });
+  };
+  const handleStairsChange = (color) => {
+    setLightColorState({ ...lightColorState, stairsColor: color.hex });
+  };
+  const handleFloorChange = (color) => {
+    setLightColorState({ ...lightColorState, floorColor: color.hex });
+  };
 
   const Container = styled.div`
-  display:flex;
-  justify-content:space-around;
-  `
+    display: flex;
+    justify-content: space-around;
+  `;
 
   const ToggleMusicModeButton = styled.button`
     background: ${musicState.color};
   `;
   const StairsContianer = styled.div`
-  width:50vw;
-    display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  background:${lightColorState.stairsColor};
-  `
+    width: 50vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: ${lightColorState.stairsColor};
+  `;
   const CouchContainer = styled.div`
-    width:50vw;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  background:${lightColorState.couchColor};
-
-  `
+    width: 50vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: ${lightColorState.couchColor};
+  `;
 
   const CouchPostButton = styled.button`
     border: 2px black solid;
-    background:white;
+    background: white;
     color: ${lightColorState.couchColor};
   `;
 
   const LivingRoomCeilingContainer = styled.div`
-    width:50vw;
-  background:${lightColorState.ceilingColor};
-    display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  
-  `
+    width: 50vw;
+    background: ${lightColorState.ceilingColor};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `;
   const CeilingPostButton = styled.button`
-      border: 2px black solid;
-    background:white;
-  color:${lightColorState.ceilingColor};
-  
-  `
+    border: 2px black solid;
+    background: white;
+    color: ${lightColorState.ceilingColor};
+  `;
 
-const StairsPostButton = styled.button`
-      border: 2px black solid;
-    background:white;
-  color:${lightColorState.stairsColor};
-`
+  const StairsPostButton = styled.button`
+    border: 2px black solid;
+    background: white;
+    color: ${lightColorState.stairsColor};
+  `;
 
-const FloorContainer = styled.div`
-  width:50vw;
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-background:${lightColorState.floorColor};
-`
-const FloorPostButton = styled.button`
-      border: 2px black solid;
-    background:white;
-  color:${lightColorState.floorColor};
-`
+  const FloorContainer = styled.div`
+    width: 50vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: ${lightColorState.floorColor};
+  `;
+  const FloorPostButton = styled.button`
+    border: 2px black solid;
+    background: white;
+    color: ${lightColorState.floorColor};
+  `;
 
   return (
     <div>
       <h3>{message.message}</h3>
 
       <Container>
-      <CouchContainer>
-        <h3>Change Couch Light Colors</h3>
-        <SketchPicker
-          color={lightColorState.couchColor}
-          onChange={handleCouchColorChange}
-        />
-        <CouchPostButton
-          onClick={() =>{
-            let newState = Array.from(lightColorState.couchColor)
-            newState.shift()
-            newState = newState.join(" ")
-            console.log(newState,"newState")
-            setLightColorState({...lightColorState,couchColor:newState})
-            updateLightColor(
-              `http://127.0.0.1:5000/api/v1/lights/couch`,
-              
-            )
-          }
-          }
-              >
-          Change Lights
-        </CouchPostButton>
-        
-      </CouchContainer>
-      <StairsContianer>
-        <h3>Change Stair Lights</h3>
-        <SketchPicker
-        color={lightColorState.stairsColor}
-        onChange={handleStairsChange}
-        />
-        <StairsPostButton
-        onClick={() => updateLightColor(`http://localhost:5555/lights/stairs`,lightColorState.stairsColor)}
-        >
-          Change Lights
-        </StairsPostButton>
-      </StairsContianer>
+        <CouchContainer>
+          <h3>Change Couch Light Colors</h3>
+          <SketchPicker
+            color={lightColorState.couchColor}
+            onChange={handleCouchColorChange}
+          />
+          <CouchPostButton
+            onClick={() => {
+              let newState = Array.from(lightColorState.couchColor);
+              newState.shift();
+              newState = newState.join("");
+              console.log(String(newState), "newState");
+              setLightColorState({ ...lightColorState, couchColor: newState });
+              updateLightColor(
+                `http://127.0.0.1:5000/api/v1/lights/couch`,
+                newState
+              );
+            }}
+          >
+            Change Lights
+          </CouchPostButton>
+        </CouchContainer>
+        <StairsContianer>
+          <h3>Change Stair Lights</h3>
+          <SketchPicker
+            color={lightColorState.stairsColor}
+            onChange={handleStairsChange}
+          />
+          <StairsPostButton
+            onClick={() =>
+              updateLightColor(
+                `http://localhost:5555/lights/stairs`,
+                lightColorState.stairsColor
+              )
+            }
+          >
+            Change Lights
+          </StairsPostButton>
+        </StairsContianer>
       </Container>
       <Container>
-      <LivingRoomCeilingContainer>
-        <h3>Change Ceiling Lights</h3>
-        <SketchPicker
-        color={lightColorState.ceilingColor}
-        onChange={handleCeilingChange}
-        />
-        <CeilingPostButton
-        onClick={() => updateLightColor(`http://localhost:5555/lights/livingroominceiling`, lightColorState.ceilingColor)}
-        >
-          Change Lights
-        </CeilingPostButton>
-      </LivingRoomCeilingContainer>
-      <FloorContainer>
-        <h3>Change Floor Lights</h3>
-        <SketchPicker
-        color={lightColorState.floorColor}
-        onChange={handleFloorChange}
-        />
-        <FloorPostButton
-        onClick={() => updateLightColor(`http://localhost:5555/lights/floor`,lightColorState.floorColor)}
-        >
-        Change Lights
-        </FloorPostButton>
-      </FloorContainer>
+        <LivingRoomCeilingContainer>
+          <h3>Change Ceiling Lights</h3>
+          <SketchPicker
+            color={lightColorState.ceilingColor}
+            onChange={handleCeilingChange}
+          />
+          <CeilingPostButton
+            onClick={() =>
+              updateLightColor(
+                `http://localhost:5555/lights/livingroominceiling`,
+                lightColorState.ceilingColor
+              )
+            }
+          >
+            Change Lights
+          </CeilingPostButton>
+        </LivingRoomCeilingContainer>
+        <FloorContainer>
+          <h3>Change Floor Lights</h3>
+          <SketchPicker
+            color={lightColorState.floorColor}
+            onChange={handleFloorChange}
+          />
+          <FloorPostButton
+            onClick={() =>
+              updateLightColor(
+                `http://localhost:5555/lights/floor`,
+                lightColorState.floorColor
+              )
+            }
+          >
+            Change Lights
+          </FloorPostButton>
+        </FloorContainer>
       </Container>
       <div>
         <h3>Toggle on the music</h3>
