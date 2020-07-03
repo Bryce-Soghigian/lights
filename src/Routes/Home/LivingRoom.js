6d507a3abddb2626ac2e42be463a745635649cf2
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-07-03 03:13:03 -0500

    redid and completed living room fe

--- src/Routes/Home/LivingRoom.js
+++ src/Routes/Home/LivingRoom.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import { SketchPicker } from "react-color";
 import AdjustLightColor from "../../Components/AdjustLightColor";
 
 const LivingRoom = () => {
@@ -36,15 +35,28 @@ const LivingRoom = () => {
     background: ${musicState.color};
   `;
 
-
+  const Title = styled.h1`
+  color:"#4A90E2";
+  margin:0;
+  `
+  const TitleContainer = styled.div`
+  text-align:center;
+  background:#e8e8e8;
+  `
 
   return (
     <div>
+    <TitleContainer>
+    <Title>Living Room Lights</Title>
+
+    </TitleContainer>
       <Container>
-      <AdjustLightColor requestUrl="http://localhost:5555/api/v1/lights/couch" lightLocation="couch"/>
-      {/* <AdjustLightColor />
-      <AdjustLightColor />
-      <AdjustLightColor /> */}
+      <AdjustLightColor requestUrl="http://localhost:5555/api/v1/lights/couch" lightLocation="Couch"/>
+      <AdjustLightColor requestUrl="http://localhost:5555/api/v1/lights/stairs" lightLocation="Stairs"/>
+      </Container>
+      <Container>
+      <AdjustLightColor requestUrl="http://localhost:5555/api/v1/lights/ceiling" lightLocation="Ceiling"/>
+      <AdjustLightColor requestUrl="http://localhost:5555/api/v1/lights/floor" lightLocation="Floor"/>
       </Container>
       <div>
         <h3>Toggle on the music</h3>
