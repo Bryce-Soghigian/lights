aa60b9870753f25201406b773ebb47e3929eaa77
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-07-03 03:39:35 -0500

    added all the backend setup for the living room

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const couchRouter = require("./Routes/couch/couchRouter")
+const floorRouter = require("./Routes/floor/floorRouter")
+const ceilingRouter = require("./Routes/ceiling/ceilingRouter")
+const stairsRouter = require("./Routes/stairs/stairsRouter")
 
 const server = express()
 const PORT = process.env.PORT || 5555;
@@ -9,4 +12,8 @@ server.listen(PORT, () => {
 });
 server.use(cors())
 server.use(express.json())
+//Routes
 server.use("/api/v1/lights/couch",couchRouter)
+server.use("/api/v1/lights/floor",floorRouter)
+server.use("/api/v1/lights/ceiling",ceilingRouter)
+server.use("/api/v1/lights/stairs",stairsRouter)
