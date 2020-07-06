100d26e7c6da7fad8aa35c24412f7c6aef39eb64
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-07-06 14:27:58 -0500

    color

--- server/Routes/stairs/stairsRouter.js
+++ server/Routes/stairs/stairsRouter.js
@@ -6,8 +6,14 @@ router.get("/:color",(req,res) => {
     color = "#" + color
     console.log(color,"COLOR in hex")
     let results = hexToRgb(color)
+    let resultsObject = {
+        red:results[0],
+        green:results[1],
+        blue:results[2]
+
+    }
     if(results !== null && color.length > 0){
-        res.status(200).json(results)
+        res.status(200).json(resultsObject)
     }else{
         res.status(500).json({message:"Error with the request or the server"})
     }
